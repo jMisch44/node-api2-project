2696d36a68648c35ad39cca56e1e433e54d6b3aa
jMisch44 <jennimisch@gmail.com>
2021-09-28 16:44:14 -0400

    get posts resopnds with 404 if the post is not found

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -76,4 +76,43 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Posts.findById(id);
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      const removedPost = await Posts.remove(id);
+      console.log(removedPost);
+      res.status(200).json(post);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post could not be removed",
+    });
+  }
+});
+
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Posts.findById(id);
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      const postComments = await Posts.findPostComments(id);
+      res.status(200).json(postComments);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The comments information could not be retrieved",
+    });
+  }
+});
+
 module.exports = router;
